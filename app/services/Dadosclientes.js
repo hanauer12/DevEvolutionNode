b66777fb0d9bd74cc68816a71fb35ed38bf9644a
Renato Hanauer <58238144+hanauer12@users.noreply.github.com>
2023-03-22 08:31:27 -0300

    Update Dadosclientes.js

--- app/services/Dadosclientes.js
+++ app/services/Dadosclientes.js
@@ -1,32 +1,50 @@
 const modeldadoscliente = require('../models/dadosclientes');
 
 module.exports = class DadosCliente {
-//funcionando criação por id
-  async create(body) {
-      const dados = await modeldadoscliente.create(body);
-      return dados;
-  }
-//deletar por id
-  async deleteOne(_idPedido) {
-      const dados = await modeldadoscliente.deleteOne({_id: _idPedido});
-      return dados;
-  }
-//att produto por id ok
-  async updateOne(_idPedido, body) {
-      const dados = await modeldadoscliente.updateOne({_id: _idPedido}, {$set: body});
-      return dados;
-  }
-// listar tudo
-  async find() {
-      const dados = await modeldadoscliente.find();
-      return dados;
-  }
 
-//listar por id 
-  async findOne(_idPedido) {
-      const dados = await modeldadoscliente.findOne({_id: _idPedido});
-      return dados;
-  }
-
-
+  //rota criar dados clientes
+    async create(body) {
+        try {
+            const dados = await modeldadoscliente.create(body);
+            return dados;
+        } catch (error) {
+            throw error;
+        }
+    }
+//rota de delete por id
+    async deleteOne(_idPedido) {
+        try {
+            const dados = await modeldadoscliente.deleteOne({_id: _idPedido});
+            return dados;
+        } catch (error) {
+            throw error;
+        }
+    }
+//rota de att por id
+    async updateOne(_idPedido, body) {
+        try {
+            const dados = await modeldadoscliente.updateOne({_id: _idPedido}, {$set: body});
+            return dados;
+        } catch (error) {
+            throw error;
+        }
+    }
+//rota para listar todos os clientes
+    async find() {
+        try {
+            const dados = await modeldadoscliente.find();
+            return dados;
+        } catch (error) {
+            throw error;
+        }
+    }
+//rota para listar ppr id  os clientes
+    async findOne(_idPedido) {
+        try {
+            const dados = await modeldadoscliente.findOne({_id: _idPedido});
+            return dados;
+        } catch (error) {
+            throw error;
+        }
+    }
 };
